cd3713ece3179e0bd9423dcfa77feeb4785b245b
pipiland2612 <batman@Duys-MacBook-Pro.local>
2024-05-17 21:42:15 +0700

    make the code cleaner

--- index.js
+++ index.js
@@ -5,28 +5,32 @@ import path from "path";
 import { fileURLToPath } from 'url';
 
 // calculation the name of the current day
-const today = new Date();
-const dayOfWeek = today.getDay();
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const dayName = daysOfWeek[dayOfWeek];
+function dayName() {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayName = daysOfWeek[dayOfWeek];
+    return dayName
+}
 
-// calculation the date of 4 days before
-const currentDate = new Date();
-currentDate.setDate(currentDate.getDate() - 4);
-const year = currentDate.getFullYear().toString().slice(2);
-const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-const day = currentDate.getDate().toString().padStart(2, '0');
-const formattedDate = `${year}-${month}-${day}`;
-
+function calculateDateFormatted(numberOfdayBefore) {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() - numberOfdayBefore);
+    const year = currentDate.getFullYear().toString();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    return formattedDate
+}
 //variables
 const apiKey = "d5d2b0f2e0f84b7aa9a102200241505";
 const port = 3000;
 const weatherUrl = "http://api.weatherapi.com/v1";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 
+//middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,81 +39,44 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", async (req, res) => {
     try {
-        // axios.all([
-        //     axios.get(`/my-url`), 
-        //     axios.get(`/my-url2`)
-        //   ])
-        //   .then(axios.spread((data1, data2) => {
-        //     // output of req.
-        //     console.log('data1', data1, 'data2', data2)
-        //   }));
-        const response = await axios.get(`${weatherUrl}/current.json?key=${apiKey}&q=Hanoi`);
-        // const history = await axios.get(`${weatherUrl}/history.json?key=${apiKey}&q=Hanoi&dt=${formattedDate}&end_dt=2024-05-14`)
-        const result = response.data;
-        // const historyData = history.data;
+        const [response1, response2] = await Promise.all([
+            axios.get(`${weatherUrl}/current.json?key=${apiKey}&q=Hanoi`),
+            axios.get(`${weatherUrl}/history.json?key=${apiKey}&q=Hanoi&dt=${calculateDateFormatted(4)}&end_dt=${calculateDateFormatted(1)}`)
+        ]);
+        const result = response1.data;
+        const historyData = response2.data;
         res.render("index.ejs", {
-            location: result.location.name,
-            dayName: dayName,
-            localTime: result.location.localtime,
-            temperature: response.data.current.temp_c,
-            conditionText: result.current.condition.text,
-            conditionIcon: result.current.condition.icon,
-            precip_mm: result.current.precip_mm,
-            humidity: result.current.humidity,
-            wind_kph: result.current.wind_kph,
-
-            // historyTempt1: historyData.forecast.forecastday[0].day.avgtemp_c,
-            // historyTempt2: historyData.forecast.forecastday[1].day.avgtemp_c,
-            // historyTempt3: historyData.forecast.forecastday[2].day.avgtemp_c,
-            // historyTempt4: historyData.forecast.forecastday[3].day.avgtemp_c,
-
             // historyIcon1: historyData.forecast.forecastday[0].day.condition.icon,
             // historyIcon2: historyData.forecast.forecastday[1].day.condition.icon,
             // historyIcon3: historyData.forecast.forecastday[2].day.condition.icon,
             // historyIcon4: historyData.forecast.forecastday[3].day.condition.icon,
+            result: result,
+            dayName: dayName(),
+            historyData: historyData,
         })
     } catch (error) {
         if (error.response) {
-            console.log('Error data:', error.response.data);
-            console.log('Error status:', error.response.status);
-            console.log('Error headers:', error.response.headers);
-        } else if (error.request) {
-            console.log('Error request:', error.request);
-        } else {
-            console.log('Error message:', error.message);
+            console.log(error)
         }
-        console.log('Error config:', error.config);
     }
 })
 
 app.post("/post", async (req, res) => {
     try {
         const location = req.body.id;
-        const response = await axios.get(`${weatherUrl}/current.json?key=${apiKey}&q=${location}`);
-        const result = response.data;
+        const [response1, response2] = await Promise.all([
+            axios.get(`${weatherUrl}/current.json?key=${apiKey}&q=${location}`),
+            axios.get(`${weatherUrl}/history.json?key=${apiKey}&q=${location}&dt=${calculateDateFormatted(4)}&end_dt=${calculateDateFormatted(1)}`)
+        ]);
+        const result = response1.data;
+        const historyData = response2.data;
         res.render("index.ejs", {
-            location: result.location.name,
-            dayName: dayName,
-            localTime: result.location.localtime,
-            temperature: response.data.current.temp_c,
-            conditionText: result.current.condition.text,
-            conditionIcon: result.current.condition.icon,
-            precip_mm: result.current.precip_mm,
-            humidity: result.current.humidity,
-            wind_kph: result.current.wind_kph,
-
+            result: result,
+            dayName: dayName(),
+            historyData: historyData,
         })
     } catch (error) {
-        if (error.response) {
-            console.log('Error data:', error.response.data);
-            console.log('Error status:', error.response.status);
-            console.log('Error headers:', error.response.headers);
-        } else if (error.request) {
-            console.log('Error request:', error.request);
-        } else {
-            console.log('Error message:', error.message);
-        }
-        console.log('Error config:', error.config);
+        console.log(error)
     }
 })
 
