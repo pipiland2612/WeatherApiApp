6ac28e94fc16bc07cfe0d2ad2e17143e741a3f5d
pipiland2612 <batman@Duys-MacBook-Pro.local>
2024-05-18 16:29:22 +0700

    update more function & reduce repetition

--- index.js
+++ index.js
@@ -3,26 +3,33 @@ import axios from "axios";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from 'url';
+import { format, parseISO } from 'date-fns';
 
-// calculation the name of the current day
-function dayName() {
-    const today = new Date();
-    const dayOfWeek = today.getDay();
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const dayName = daysOfWeek[dayOfWeek];
-    return dayName
-}
-
-function calculateDateFormatted(numberOfdayBefore) {
+// Function to calculate formatted date
+function calculateDateFormatted(numberOfDaysBefore) {
     const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() - numberOfdayBefore);
+    currentDate.setDate(currentDate.getDate() - numberOfDaysBefore);
     const year = currentDate.getFullYear().toString();
     const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
     const day = currentDate.getDate().toString().padStart(2, '0');
-    const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate
+    return `${year}-${month}-${day}`;
 }
-//variables
+
+// function generateHistoryDayname(numberOfDaysBack) {
+//     let historyDayName = {}
+//     for (let i = numberOfDaysBack; i > 0; i--) {
+//         historyDayName[`historyDayName${i}`] = getDayOfWeek(calculateDateFormatted(i));
+//     }
+//     return historyDayName
+// }
+
+// Function to get the day of the week
+function getDayOfWeek(dateString) {
+    const date = parseISO(dateString);
+    return format(date, 'EEEE').substring(0, 3);
+}
+
+// Variables
 const apiKey = "d5d2b0f2e0f84b7aa9a102200241505";
 const port = 3000;
 const weatherUrl = "http://api.weatherapi.com/v1";
@@ -30,13 +37,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
-//middleware
+// Middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Root route
 app.get("/", async (req, res) => {
     try {
         const [response1, response2] = await Promise.all([
@@ -45,22 +53,26 @@ app.get("/", async (req, res) => {
         ]);
         const result = response1.data;
         const historyData = response2.data;
-        res.render("index.ejs", {
-            // historyIcon1: historyData.forecast.forecastday[0].day.condition.icon,
-            // historyIcon2: historyData.forecast.forecastday[1].day.condition.icon,
-            // historyIcon3: historyData.forecast.forecastday[2].day.condition.icon,
-            // historyIcon4: historyData.forecast.forecastday[3].day.condition.icon,
-            result: result,
-            dayName: dayName(),
-            historyData: historyData,
-        })
-    } catch (error) {
-        if (error.response) {
-            console.log(error)
+        const historyDays = [];
+        for (let i = 0; i < 4; i++) {
+            historyDays.push({
+                dayName: getDayOfWeek(calculateDateFormatted(4 - i)),
+                avgTemp: historyData.forecast.forecastday[i].day.avgtemp_c,
+                iconClass: ['bx-cloud', 'bx-sun', 'bx-cloud-rain', 'bx-cloud-drizzle'][i]
+            });
         }
+        res.render("index.ejs", {
+            result: result,
+            dayName: getDayOfWeek(calculateDateFormatted(0)),
+            historyData: historyData,
+            historyDays: historyDays
+        });
+    } catch (error) {
+        console.error(error);
     }
-})
+});
 
+// Post route
 app.post("/post", async (req, res) => {
     try {
         const location = req.body.id;
@@ -70,17 +82,30 @@ app.post("/post", async (req, res) => {
         ]);
         const result = response1.data;
         const historyData = response2.data;
+        // const historyDayNames = generateHistoryDayname(4)
+        const historyDays = [];
+        for (let i = 0; i < 4; i++) {
+            historyDays.push({
+                dayName: getDayOfWeek(calculateDateFormatted(4 - i)),
+                avgTemp: historyData.forecast.forecastday[i].day.avgtemp_c,
+                iconClass: ['bx-cloud', 'bx-sun', 'bx-cloud-rain', 'bx-cloud-drizzle'][i]
+            });
+        }
         res.render("index.ejs", {
             result: result,
-            dayName: dayName(),
+            dayName: getDayOfWeek(calculateDateFormatted(0)),
             historyData: historyData,
-        })
+            // ...historyDayNames
+            historyDays: historyDays
+
+        });
     } catch (error) {
-        console.log(error)
+        console.error(error);
     }
-})
+});
 
-
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
